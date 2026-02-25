35a8c3cc465605c62aee007f54afb4e340e66f6d
GhadyOmar <90251825+GhadyOmar@users.noreply.github.com>
2026-02-26 01:53:19 +0300

    Initial Commit

---
+++ tamakkan/tamakkanApp.swift
@@ -0,0 +1,17 @@
+//
+//  tamakkanApp.swift
+//  tamakkan
+//
+//  Created by Ghady Al Omar on 09/09/1447 AH.
+//
+
+import SwiftUI
+
+@main
+struct tamakkanApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ tamakkanTests/tamakkanTests.swift
@@ -0,0 +1,17 @@
+//
+//  tamakkanTests.swift
+//  tamakkanTests
+//
+//  Created by Ghady Al Omar on 09/09/1447 AH.
+//
+
+import Testing
+@testable import tamakkan
+
+struct tamakkanTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
